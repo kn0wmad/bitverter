8fa1604a380861e668f49e50f50d55782756f327
Hunter Trujillo <cryptoquick@pm.me>
2020-09-26 17:24:07 -0600

    Nomics price request. Config file.

--- README.md
+++ README.md
@@ -17,9 +17,19 @@ needs:
 - general
 
   - <https://cheats.rs>
+  - <https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html>
 
 - dependencies
 
-  - <https://actix.rs/docs/request/>
-  - <https://github.com/seanmonstar/reqwest>
-  - <https://maud.lambda.xyz/web-frameworks.html>
+  - web framework: <https://actix.rs/docs/request/>
+  - templating: <https://maud.lambda.xyz/web-frameworks.html>
+  - make api requests: <https://github.com/seanmonstar/reqwest>
+  - async/await: <https://github.com/async-rs/async-std>
+
+- tools
+
+  - cargo add, etc.: <https://github.com/killercup/cargo-edit>
+
+- apis
+
+  - <https://docs.nomics.com#tag/Currencies>
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use maud::html;
 use serde::Deserialize;
+use tokio::fs;
 
 async fn index() -> impl Responder {
     let body = html! {
@@ -16,14 +17,40 @@ async fn index() -> impl Responder {
 }
 
 #[derive(Deserialize)]
-struct ConversionParmams {
+struct ConvertParams {
     amount: String,
 }
 
-async fn conversion(params: web::Query<ConversionParmams>) -> impl Responder {
+#[derive(Deserialize)]
+struct Config {
+    nomics_api_key: String,
+}
+
+#[derive(Deserialize)]
+struct NomicsPriceResponse {
+    price: String,
+}
+
+async fn convert(params: web::Query<ConvertParams>) -> impl Responder {
+    let config_str = fs::read("config.json").await.unwrap();
+    let config: Config = serde_json::from_slice(&config_str).unwrap();
+
+    let btc_usd_price_request_url = format!(
+        "https://api.nomics.com/v1/currencies/ticker?key={}&ids=BTC&interval=1d&convert=USD",
+        config.nomics_api_key
+    );
+
+    let response = reqwest::get(&btc_usd_price_request_url).await.unwrap();
+    let btc_usd_price: NomicsPriceResponse = response.json().await.unwrap();
+
+    let price = btc_usd_price.price.parse::<f64>().unwrap();
+    let amount = params.amount.parse::<f64>().unwrap();
+
+    let result = price * amount;
+
     let body = html! {
         h1 { "USD -> BTC" }
-        p { (params.amount) " BTC" }
+        p { (result) " BTC" }
     };
 
     HttpResponse::Ok().body(body.into_string())
@@ -34,7 +61,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/convert", web::get().to(conversion))
+            .route("/convert", web::get().to(convert))
     })
     .bind("127.0.0.1:8080")?
     .run()
