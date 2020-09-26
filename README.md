91b460ab2124fc1ae761a3f8c4d789fdafa64df3
Hunter Trujillo <cryptoquick@pm.me>
2020-09-26 14:56:25 -0600

    Initial commit.

--- README.md
+++
@@ -1,4 +0,0 @@
-##  Bitverter - A simple Bitcoin conversion tool  ##
-
-
-Work has just begun, please check back or feel free to reach out with any questions or comments :)
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
-// bitverter
+#![feature(decl_macro)]
+#![feature(proc_macro_hygiene)]
 
-use std::io;
-//use clap::{App};
+use maud::{html, Markup};
+use rocket::{get, http::RawStr, routes};
 
+#[get("/")]
+fn landing<'a>() -> Markup {
+    html! {
+        h1 { "bitverter" }
+        p { "how many btc can i buy wit usd"}
+        form action="/convert" method="get" {
+            input name="amount" type="number" {}
+            input type="submit" value="convert!!" {}
+        }
+    }
+}
+
+#[get("/convert?<amount>")]
+fn conversion<'a>(amount: &RawStr) -> Markup {
+    html! {
+        h1 { "USD -> BTC" }
+        p { (amount) " BTC" }
+    }
+}
 
 fn main() {
-    println!("Hello!  Welcome to Bitverter!!");
-    println!("Hola! Bienvenidos a Bitverter!!");
-
-    loop{
-        println!("Choose denomination to convert from (Please enter: sats, bits, mbtc, btc):");
-
-        println!("Choose denomination to convert to (Please enter: sats, bits, mbtc, btc):");
-
-        println!("Enter amount:");
-
-    // create mutable variable named btc and bind to a new, empty instance of a String
-        let mut btc = String::new();
-        
-    // call stdin function from the io module
-        io::stdin().read_line(&mut btc).expect("Failed to read your input");
-
-    // calls read_line method on the standard input handle to get input from user
-    // also passes one argument to read_line - &mut btc
-    // read_line takes standard input and places it into a string
-
-    // bind btc to expression btc.trim.parse - trim will remove whitespace
-    // u32 can only contain numerical characters
-        let btc: u32 = match btc.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-    }
+    rocket::ignite()
+        .mount("/", routes![landing, conversion])
+        .launch();
 }
