d87820b35a46521091e150a925ea791e6c2de447
kn0wmad <kn0wmad@protonmail.com>
2020-10-19 16:58:35 -0600

    Reworking...

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
-// bitverter
-
 use std::io;
 //use clap::{App};
 
+enum Denomination {
+    sats,
+    bits,
+    mbtc,
+    btc,
+}
 
 fn main() {
     println!("Hello!  Welcome to Bitverter!!");
     println!("Hola! Bienvenidos a Bitverter!!");
+    
+    // create mutable variable named user_denom and bind to a new, empty instance of a String
 
-    loop{
-        println!("Choose denomination to convert from (Please enter: sats, bits, mbtc, btc):");
+        let mut user_denom = String::new();
+        let mut desired_denom: String = String::new();
+        let mut user_amount: String = String::new();
 
-        println!("Choose denomination to convert to (Please enter: sats, bits, mbtc, btc):");
+        use crate::Denomination::*;
 
-        println!("Enter amount:");
+        impl Denomination {
+            fn as_str(&self) -> &'static str {
+                match *self {
+                    Denomination::sats => "sats",
+                    Denomination::bits => "bits",
+                    Denomination::mbtc => "mbtc",
+                    Denomination::btc => "btc"
+                }
+            }
+        }
+    /* call stdin function from the io module & call read_line method on the standard input handle to get input from user
+    also passes one argument to read_line - &mut VARIABLE
+    read_line takes standard input and places it into a string */
 
-    // create mutable variable named btc and bind to a new, empty instance of a String
-        let mut btc = String::new();
-        
-    // call stdin function from the io module
-        io::stdin().read_line(&mut btc).expect("Failed to read your input");
+        println!("Your denomination:");
+        io::stdin().read_line(&mut user_denom).expect("Please enter sats, bits, mbtc, or btc");
 
-    // calls read_line method on the standard input handle to get input from user
-    // also passes one argument to read_line - &mut btc
-    // read_line takes standard input and places it into a string
+        match user_denom.as_str() {
+            "sats" => println!("Enter amount in Sats:"),
+            "bits" => println!("Enter amount in Bits:"),
+            "mbtc" => println!("Enter amount in mBTC:"),
+            "btc" => println!("Enter amount in BTC:"),
+        }
 
-    // bind btc to expression btc.trim.parse - trim will remove whitespace
-    // u32 can only contain numerical characters
-        let btc: u32 = match btc.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-    }
+        /*
+        if {
+
+            println!("Desired Denomination:");
+            io::stdin().read_line(&mut desired_denom).expect("Please enter sats, bits, mbtc, or btc");
+
+            if desired_denom.contains("sats") || desired_denom.contains("bits") || desired_denom.contains("mbtc") || desired_denom.contains("btc") {
+                
+                println!("Enter amount:");
+                io::stdin().read_line(&mut user_amount).expect("Please enter the amount, for example: 0.042");
+
+                let user_amount: f64 = match user_amount.parse() {
+                    Ok(num) => num,
+                    Err(_) => 0.0,
+                };
+
+                let output = user_amount / 10.0;
+                println!("Value: {:}", output);
+                
+            } else {
+                println!("Invalid input");
+            }
+            
+        } else {
+            println!("Invalid input");
+        } */
 }
