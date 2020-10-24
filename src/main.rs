9f5875d6d6e838388a93756c01ee7a869b82ca46
kn0wmad <kn0wmad@protonmail.com>
2020-10-24 17:03:42 -0600

    Removed all garbage code and cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,48 @@
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
 
-/*#[macro_use]
-extern crate custom_derive;
-#[macro_use]
-extern crate enum_derive;*/
-
 use std::io;
+use std::str::FromStr;
 //use clap::{App};
 
 // Create and implement an enumeration Denomination, with 4 possible values.  custom_derive to use as type &str
 
 enum Denomination {
-    sats(String),
-    bits(String),
-    mBTC(String),
-    BTC(String)
+    sats,
+    bits,
+    mBTC,
+    BTC
 }
 
-impl Denomination {
-         fn which_denom(&self) -> &String {
-            match *self {
-                Denomination::sats(ref msg) |
-                Denomination::bits(ref msg) |
-                Denomination::mBTC(ref msg) |
-                Denomination::BTC(ref msg) => msg
-                }
-            }
+impl FromStr for Denomination {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "sats" => Self::sats,
+            "bits" => Self::bits,
+            "mBTC" => Self::mBTC,
+            "BTC" => Self::BTC
         }
+    }
+}
 
 fn main() {
     // Welcome user
     println!("Hello!  Welcome to Bitverter!!");
     println!("Hola! Bienvenidos a Bitverter!!");
 
-    // Create mutable variable named user_denom and bind to a new, empty instance of a String
-    let mut user_denom = Denomination::sats(String::from("sats"));
-    //println!("{}", user_denom.which_denom());
+    loop {
+        println!("Enter your current denomination (sats, bits, mBTC, or BTC): ");
 
-    //user_denom = Denomination::bits(String::from("bits"));
-    //println!("{}", user_denom.which_denom());
-
-    //user_denom = Denomination::mBTC(String::from("mBTC"));
-    //println!("{}", user_denom.which_denom());
-
-    //user_denom = Denomination::BTC(String::from("BTC"));
-    //println!("{}", user_denom.which_denom());
-
-    // Check if user input matches any value in enum Denomination
-    /*match io::stdin().read_line(&mut user_denom) {
-        if {
-            Ok(String) "sats" => println!("Did you provide a valid denomination? {}", user_denom.which_denom())
-        }
-    }*/
-    //println!("Please enter your current denomination - sats, bits, mBTC, or BTC:");
-}
-
-    //println!("Is user_denom a Denomination? {}", user_denom.which_denom());
-    
-    
-/*
-    assert_eq!("sats", user_denom.as_str());
-
-    match io::stdin().read_line(&mut user_denom){
-        Ok(_) => (
-            println!("Successs! You have selected: {}", user_denom.to_lowercase())
-        ),
-        Err(e) => println!("Error. Please enter sats, bits, mBTC, or BTC: {}", e)
-    }
-
-        let mut desired_denom: String = String::new();
-        let mut user_amount: String = String::new();
-
-        use crate::Denomination::*;
-
-    /* call stdin function from the io module & call read_line method on the standard input handle to get input from user
-    also passes one argument to read_line - &mut VARIABLE
-    read_line takes standard input and places it into a string */
-
+        // Create mutable variable user_denom and bind to a new, empty instance of a String
+        let mut user_denom = String::new();
         
-        io::stdin().read_line(&mut user_denom).expect("Please enter sats, bits, mBTC, or BTC");
-
-        match user_denom.as_str() {
-            "sats" => println!("Enter amount in sats:"),
-            "bits" => println!("Enter amount in bits:"),
-            "mBTC" => println!("Enter amount in mBTC:"),
-            "BTC" => println!("Enter amount in BTC:"),
-            _ => panic!("Unexpected entry!"),
-        }
-*/
-        /*
-        if {
-
-            println!("Desired Denomination:");
-            io::stdin().read_line(&mut desired_denom).expect("Please enter sats, bits, mBTC, or BTC");
-
-            if desired_denom.contains("sats") || desired_denom.contains("bits") || desired_denom.contains("mBTC") || desired_denom.contains("BTC") {
-                
-                println!("Enter amount:");
-                io::stdin().read_line(&mut user_amount).expect("Please enter the amount, for example: 0.042");
-
-                let user_amount: f64 = match user_amount.parse() {
-                    Ok(num) => num,
-                    Err(_) => 0.0,
-                };
-
-                let output = user_amount / 10.0;
-                println!("Value: {:}", output);
-                
-            } else {
-                println!("Invalid input");
-            }
-            
-        } else {
-            println!("Invalid input");
-        } */
+        // Call read_line method to get user input, pass the &mut user_denom argument to read_line
+        // read_line takes standard input and places it into a String
+        io::stdin().read_line(&mut user_denom)
+            .expect("Failed to read your input");
+        
+        // bind user_denom to expression std::env::args().next().unwrap().parse().unwrap()
+        let user_denom: Denomination = std::env::args().next().unwrap().parse().unwrap();
+    }
+}
