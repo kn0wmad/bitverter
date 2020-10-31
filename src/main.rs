4b76099b690b18b7fa1841482760c55cd854a948
kn0wmad <kn0wmad@protonmail.com>
2020-10-30 21:48:00 -0600

    Probably don't look at this

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ enum Denomination {
 impl FromStr for Denomination {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        println!("Debugging... {:?}", s);
         match s {
             "sats" => Ok(Self::sats),
             "bits" => Ok(Self::bits),
@@ -40,21 +41,28 @@ fn main() {
         
     // Call read_line method to get user input, pass the &mut starting_denom argument to read_line
     // read_line takes standard input and places it into a String
-        io::stdin().read_line(&mut starting_denom).expect("Failed to read your input");
+        match io::stdin().read_line(&mut starting_denom) {
+            Ok(String) => {
+                println!("You have selected {}", starting_denom);
+            }
+            Err(error) => println!("Error: {}", error),
+        }
         
     // bind starting_denom to expression parsing the string
-        let starting_denom: Denomination = std::env::args().next().unwrap().parse().unwrap();
-      
-        println!("You have selected: {:?}", &starting_denom);
+        if starting_denom == "sats" {
+            println!("You have selected sats??");
+        }
+
+        //println!("You have selected: {:?}", &starting_denom);
 
     // Create mutable variable desired_denom, bind to new, empty instance of a String, 
     // and request input from user
-        let mut desired_denom = String::new();
+        /*let mut desired_denom = String::new();
 
         println!("Enter your current denomination (sats, bits, mBTC, or BTC): ");
 
         io::stdin().read_line(&mut desired_denom).expect("Failed to read your input");
 
-        println!("You have entered {}", desired_denom);
+        println!("You have entered {}", desired_denom);*/
     }
 }
