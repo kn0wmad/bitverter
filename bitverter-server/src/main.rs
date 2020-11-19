7f86d164f544ce898754a33e9dc883c33c2a733c
kn0wmad <kn0wmad@protonmail.com>
2020-11-18 21:13:13 -0700

    First Maud commit

--- bitverter-server/src/main.rs
+++ bitverter-server/src/main.rs
@@ -1,4 +1,5 @@
 use warp::Filter;
+use maud::{ DOCTYPE, html, Markup };
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
@@ -6,13 +7,16 @@ extern crate pretty_env_logger;
 
 #[tokio::main]
 async fn main() {
+
     pretty_env_logger::init();
 
     let index = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("./index.html"));
 
-    let site = warp::path("bitverter-server").and(warp::fs::dir("./bitverter-server/"));
+    let site = 
+        warp::path("bitverter-server")
+        .and(warp::fs::dir("./bitverter-server/"));
 
     let routes = index.or(site);
 
@@ -22,4 +26,13 @@ async fn main() {
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+fn header(page_title: &str) -> Markup {
+    html! {
+        (DOCTYPE)
+        meta charset="utf-8";
+        meta name="viewport" content="width=device-width, initial-scale=1";
+        title { (page_title) }
+    }
 }
