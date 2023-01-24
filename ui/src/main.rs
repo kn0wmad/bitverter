04599fe2e4d0b299d64dc6848ca5cd0c4216a1af
kn0wmad <kn0wmad@protonmail.com>
2023-01-23 19:03:09 -0700

    Dioxus functional

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,3 +1,17 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, Dioxus!");
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, Bitverter!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, Bitverter!"
+        }
+    })
 }
