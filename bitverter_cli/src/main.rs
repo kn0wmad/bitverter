3327db582159b367686c976f0270ad0a68f447b6
notmike-5 <notmike-5@users.noreply.github.com>
2023-05-18 16:41:39 -0600

    refactored a bit

--- bitverter_cli/src/main.rs
+++ bitverter_cli/src/main.rs
@@ -1,82 +1,74 @@
-use std::{ io, rc::Rc };
+use std::io::{stdin, stdout, Write};
 
-// Take a value and apply the correct conversion to sats, based on denomination provided
+/* bitverter - A user-driven Bitcoin unit converter */
 
-
+// helper for getting units/denomination
 fn get_denom_from_input() -> String {
-    let mut denom;
+    let mut denom = String::new();
     loop {
-        denom = String::new();
-        println!("1) Sats,\n2) Bits,\n3) mBTC,\n4) BTC: ");
-        io::stdin().read_line(&mut denom).expect("Failed to read your input");
+        print!(
+            "1) sats\t2) finneys\n\
+	     3) bits\t4) uBTC\n\
+	     5) mBTC\t6) BTC\n\n\
+	     => "
+        );
+        stdout().flush().unwrap();
+        stdin()
+            .read_line(&mut denom)
+            .expect("Failed to read your input");
         denom = match denom.trim().to_lowercase().as_str() {
-            "1" | "sats" => "sats".to_owned(),
-            "2" | "bits" => "bits".to_owned(),
-            "3" | "mbtc" => "mbtc".to_owned(),
-            "4" | "btc" => "btc".to_owned(),
+            "1" | "s" | "sat" | "sats" => "sats".to_owned(),
+            "2" | "f" | "finneys" => "finneys".to_owned(),
+            "3" | "bit" => "bits".to_owned(),
+            "4" | "u" | "ubtc" | "ubtcs" => "ubtc".to_owned(),
+            "5" | "m" | "mbtc" | "mbtcs" => "mbtc".to_owned(),
+            "6" | "b" | "btc" | "btcs" => "btc".to_owned(),
             _ => {
-                eprintln!("Must be one of the 4 options listed above");
+                println!("You must select an option listed below!\n");
                 continue;
-            },
+            }
         };
-        println!("You have selected: {:?}", denom);
+        println!("You have selected: {}\n", denom);
         break;
     }
+
     denom
 }
 
-fn from_denom_to_sats(starting_denom: &str, val: Rc<String>) -> Option<Rc<String>> {
-    match starting_denom.to_lowercase().trim() {
-        "sats" => Some(val),
-        "bits" => Some(bitverter_lib::bits_to_sats(val)),
-        "mbtc" => Some(bitverter_lib::mbtc_to_sats(val)),
-        "btc" => Some(bitverter_lib::btc_to_sats(val)),
-        _ => None
-    }
+// helper for getting a quantity of units
+fn get_quantity(units: &str) -> f64 {
+    let mut s = String::new();
+
+    println!("Input your current value in {:?}", units.to_lowercase());
+    stdin()
+        .read_line(&mut s)
+        .expect("\nFailed to read your input!");
+
+    let quantity: f64 = s.trim().parse().unwrap();
+
+    quantity
 }
 
-// Take sats calculated and denomination provided and return a string based on denomination provided
-
-fn to_string_from_denom(desired_denom: &str, sats: Rc<String>) -> Option<String> {
-    match desired_denom.to_lowercase().trim() {
-        "sats" => Some(format!("{} {}", sats, desired_denom)),
-        "bits" => Some(format!("{} {}", bitverter_lib::sats_to_bits(sats), desired_denom)),
-        "mbtc" => Some(format!("{} {}", bitverter_lib::sats_to_mbtc(sats), desired_denom)),
-        "btc" => Some(format!("{} {}", bitverter_lib::sats_to_btc(sats), desired_denom)),
-        _ => None
-    }
-}
-
+// return a quote from user-provided (base,quote,quantity)
 fn main() {
-
-// Welcome user
+    // welcome user
     println!("Hello!  Welcome to Bitverter!!");
     println!("Hola! Bienvenidos a Bitverter!!");
     println!("Привет! Добро пожаловать в Bitverter!!\n");
 
-// Create mutable variable starting_denom and bind to a new, empty instance of a String
-// Call read_line method to get user input, pass the &mut starting_denom argument to read_line
-// read_line takes standard input and places it into a String - trim the result and print back to user
+    // get base unit
     println!("Enter your current denomination");
-    let starting_denom = get_denom_from_input();
+    let base_unit = get_denom_from_input();
 
-// Repeat above process to request current value in the user's starting denomination, then perform sats conversion
-    let mut starting_value = String::new();
+    // get quantity
+    let quantity = get_quantity(&base_unit);
 
-    println!("Input your current value in {:?}", starting_denom.to_lowercase());
-    io::stdin().read_line(&mut starting_value).expect("Failed to read your input");
+    // get quote unit
+    println!("\nInput your desired denomination");
+    let quote_unit = get_denom_from_input();
 
-    let starting_value = Rc::new(starting_value.trim().to_owned());
-    let starting_sats = from_denom_to_sats(&starting_denom, Rc::clone(&starting_value))
-        .expect("Please enter a valid denomination");
+    // convert
+    let quote = bitverter_lib::convert(quantity, &base_unit, &quote_unit);
 
-// Repeat process from above again to get user's desired denomination
-    println!("Input your desired denomination");
-    let desired_denom = get_denom_from_input();
-
-// Take string of calculated value and return the full conversion to the user
-
-    let desired_value = to_string_from_denom(&desired_denom, Rc::clone(&starting_sats))
-        .expect("Please enter a valid denomination");
-    println!("{} {} = {}", starting_value, starting_denom, desired_value);
+    println!("{} {} = {} {}", quantity, &base_unit, quote, &quote_unit);
 }
