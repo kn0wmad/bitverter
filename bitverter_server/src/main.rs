6c073557c526da9337f533d38d5f33c8a5398236
kn0wmad <kn0wmad@protonmail.com>
2025-02-09 20:43:49 -0700

    Scaffolding for Axum

--- bitverter_server/src/main.rs
+++ bitverter_server/src/main.rs
@@ -1,26 +1,45 @@
 use std::error::Error;
 use std::net::SocketAddr;
 
-use axum::{routing::get, routing::get_service, Router};
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+    Router,
+};
 use tower_http::services::ServeDir;
 
+#[derive(Debug)]
+struct BvParams {
+    denomination: Option<String>,
+}
+
+// Handler for Input
+async fn handler_bv_input(Query(params): Query<BvParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_bv_input - {params:?}", "HANDLER");
+
+    let denomination = params.denomination.as_deref().unwrap_or("Bitcoin");
+    Html(format!("Denomination: {denomination}"))
+}
+
+// Test API
 async fn test() -> &'static str {
-    "Welcome to Bitverter!"
+    "Welcome to Bitverter! \nThis is the testing API"
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // build application with a single route
-    let app = Router::new()
+    let routes_bitverter = Router::new()
         .fallback_service(get_service(ServeDir::new("./site")))
         .route("/api/test", get(test).post(test));
+
+    // Listen on localhost
     let addr = SocketAddr::from(([127, 0, 0, 1], 3333));
     println!("Listening on {}", addr);
-    let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, app).await?;
-    Ok(())
 
-    // // run app with hyper, listening globally on port 3333
-    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
-    // axum::serve(listener, app).await.unwrap();
+    // Start server and bind to above address
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, routes_bitverter).await?;
+    Ok(())
 }
