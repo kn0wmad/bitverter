b9b61dd47d69cb25d5f2cef1aaa8da17c2a81c2b
kn0wmad <kn0wmad@protonmail.com>
2025-06-05 17:54:15 -0600

    Add basic tests
    Co-authored-by: Jason Ribble <git@j6r.org>

--- bitverter_server/src/main.rs
+++ bitverter_server/src/main.rs
@@ -2,7 +2,6 @@
 use std::error::Error;
 use std::net::SocketAddr;
 use axum::{routing::{get, get_service, post}, Form, Router};
-use axum_macros::debug_handler;
 use serde::Deserialize;
 use tower_http::services::ServeDir;
 
@@ -14,25 +13,23 @@ struct Conversion {
 }
 
 /// Test API
-async fn test() -> &'static str {
+async fn hander_test_route() -> &'static str {
     "Welcome to Bitverter! \nThis is the testing API"
 }
 
 /// Conversion function using bitverter_lib
 /// Take user input from web form
-#[debug_handler]
 async fn convert(form: Form<Conversion>) -> String {
     let result = bitverter_lib::convert(form.quantity, &form.base_unit, &form.quote_unit);
     format!("{}", result)
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+pub async fn run_server() -> Result<(), Box<dyn Error>> {
     // build application with a single route
     let router = Router::new()
         .fallback_service(get_service(ServeDir::new("./site")))
         .route("/api/convert", post(convert))
-        .route("/api/test", get(test));
+        .route("/api/test", get(hander_test_route));
 
     // Listen on localhost
     let addr = SocketAddr::from(([127, 0, 0, 1], 3333));
@@ -42,4 +39,41 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+   run_server().await
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn should_return_message_when_calling_test_handler() {
+        assert!(hander_test_route().await == "Welcome to Bitverter! \nThis is the testing API");
+    }
+
+    #[tokio::test]
+    async fn should_return_ok_when_calling_test_route() {
+        reqwest::get("http://localhost:3333/api/test").await.unwrap().text().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn should_return_100_000_000_sats_when_given_1_bitcoin() {
+        let client = reqwest::Client::new();
+
+         let form_data = [
+             ("quantity", "1"),
+             ("base_unit", "btc"),
+             ("quote_unit", "sats"),
+         ];
+
+         let res = client.post("http://localhost:3333/api/convert")
+             .form(&form_data).send().await.unwrap();
+
+        let conversion_amount = res.text().await.unwrap();
+        assert_eq!(conversion_amount, 100000000.to_string());
+    }
 }
